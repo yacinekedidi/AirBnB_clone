942d2a2a7136e74d76476cec453886f7e5233e5a
yacinekedidi <kedidiyacine@gmail.com>
2020-06-30 14:35:02 +0100

    Py

---
+++ models/base_model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Module
+
+
+
+"""
+import uuid
+import datetime
+import models
+
+
+class BaseModel:
+    """
+    parent of all our classes
+    """
+    def __init__(self, *args, **kwargs):
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
+        else:
+            tf = '%Y-%m-%dT%H:%M:%S.%f'
+            for k, v in kwargs.items():
+                if k == "created_at":
+                    self.__dict__[k] = datetime.datetime.strptime(v,  tf)
+                elif k == "updated_at":
+                    self.__dict__[k] = datetime.datetime.strptime(v, tf)
+                else:
+                    self.__dict__[k] = v
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """
+         updates the public instance attribute
+         updated_at with the current datetime
+        """
+        self.updated_at = datetime.datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing
+        all keys/values of __dict__ of the instance
+        """
+        dict = {"__class__": self.__class__.__name__}
+        dict.update(self.__dict__)
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
+
+        return dict
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Unittest for class BaseModel([..])
+"""
+
+import unittest
+from models.base_model import BaseModel
+import models
+
+class TestBaseModel(unittest.TestCase):
+    """class"""
+
+    def test_args(self):
+        """test without args"""
+
+        b1 = BaseModel()
+        b3 = BaseModel(name="Ali")
+        b2 = BaseModel(id='Betty')
+        self.assertRaises(AttributeError)
+        self.assertEqual(b2.id, "Betty")
+        self.assertTrue(hasattr(b2, "id"))
+        self.assertTrue(hasattr(b1, "created_at"))
+        self.assertTrue(hasattr(b1, "updated_at"))
+        self.assertFalse(hasattr(b2, "created_at"))
+        self.assertFalse(b1.id == b2.id)
+
+
+    def test_str(self):
+        """ test print """
+
+        b1 = BaseModel()
+        b2 = BaseModel(id='Betty')
+        s = "[{}] ({}) {}".format("BaseModel", b1.id, b1.__dict__)
+        self.assertEqual(print(s), print(b1))
+
+        s = "[{}] ({}) {}".format("BaseModel", b2.id, b2.__dict__)
+        self.assertEqual(print(s), print(b2))
+
+
+    def test_dict(self):
+        """ to_dict """
+
+        b1 = BaseModel()
+        d = {"__class__": b1.__class__.__name__}
+        d.update(b1.__dict__)
+        d["created_at"] = b1.created_at.isoformat()
+        d["updated_at"] = b1.updated_at.isoformat()
+        self.assertEqual(print(d), print(b1.to_dict()))
+
+
+    def test_file(self):
+        """ test storege """
+
+        x = 0
+        b1 = BaseModel()
+        b1.save()
+        models.storage.reload()
+        d = models.storage.all()
+        for k in d.keys():
+            if b1.id in k:
+                x = 1
+
+        self.assertEqual(x, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
