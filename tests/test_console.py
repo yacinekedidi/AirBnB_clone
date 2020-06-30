e3467082c1b93fb4c2c4684139a3244ac7b5d845
yacinekedidi <kedidiyacine@gmail.com>
2020-06-30 19:09:11 +0100

    cons_test

---
+++ tests/test_console.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Unittest for class BaseModel([..])
+"""
+import unittest
+from console import HBNBCommand
+from models.engine.file_storage import FileStorage
+
+
+class TestConsole(unittest.TestCase):
+    """test"""
+    pass
