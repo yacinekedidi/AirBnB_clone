653e80fcf25fb8ec4412768328dabbbe6ed6b545
yacinekedidi <kedidiyacine@gmail.com>
2020-06-30 15:32:55 +0100

    test

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unittest for class FileStorage([..])
+"""
+
+import unittest
+from models.engine.file_storage import FileStorage
+import models
+
+
+class TestFileStorage(unittest.TestCase):
+    """test"""
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
