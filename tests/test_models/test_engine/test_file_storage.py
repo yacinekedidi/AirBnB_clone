f76b62727d87d5cdbc4e19d3da73dd8f573e9a65
kefitaib <taibkefi@yahoo.fr>
2020-06-30 12:53:34 -0700

    fix

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,12 +4,33 @@
 
 import unittest
 from models.engine.file_storage import FileStorage
-import models
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
     """test"""
-    pass
+
+    def test_attribute(self):
+        """ function all """
+
+        f = FileStorage()
+        self.assertIsInstance(f._FileStorage__objects, dict)
+        self.assertIsInstance(f._FileStorage__file_path, str)
+
+    def test_all(self):
+        """ test all """
+
+        f = FileStorage()
+        d = f.all()
+        self.assertIsInstance(d, dict)
+
+    def test_new(self):
+        """ test new """
+
+        b = BaseModel()
+        f = FileStorage()
+        f.new(b)
+        self.assertEqual(b, f.all()["BaseModel" + "." + b.id])
 
 if __name__ == '__main__':
     unittest.main()
